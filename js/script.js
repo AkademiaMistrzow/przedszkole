9f3ca9e0de6c4b2cbcfdd3d670db7c6626fe3e7f
Jed <jedrek90@gmail.com>
2024-03-03 14:34:11 +0100

    fixed menu

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-const mainMenu = document.querySelector(".mainMenu");
-const closeMenu = document.querySelector(".closeMenu");
-const openMenu = document.querySelector(".openMenu");
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const menuContact = document.querySelector('#menu-contact');
 
-openMenu.addEventListener("click", show);
-closeMenu.addEventListener("click", close);
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+menuContact.addEventListener('click', close);
 
 function show() {
-  mainMenu.style.display = "flex";
+    mainMenu.style.display = 'flex';
 }
 function close() {
-  mainMenu.style.display = "none";
+    mainMenu.style.display = 'none';
 }
